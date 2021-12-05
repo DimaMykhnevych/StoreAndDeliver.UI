e4504633092f57ff10dfdcc83236821acaf64d13
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2021-12-05 14:08:03 +0200

    Add deployed backend routes

--- StoreAndDeliverUI/src/app/core/settings.ts
+++ StoreAndDeliverUI/src/app/core/settings.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
-  public static apiHost = 'http://localhost/api';
-  // public static apiHost = 'https://storeanddeliver.azurewebsites.net/api';
+  // public static apiHost = 'http://localhost/api';
+  public static apiHost = 'https://storeanddeliver.azurewebsites.net/api';
   public static hubHost = 'https://localhost:5001';
   public static confirmEmailPath = 'http://localhost:4200/emailConfirmation';
 }
